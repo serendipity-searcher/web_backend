e04f2e03ff1bbd7700c86818a916aeb3202314d7
valevo <valevogelmann@gmail.com>
2025-06-07 13:13:52 +0200

    Put line with embedding of texts back in

--- data/MKG/INSTALL_MKG.sh
+++ data/MKG/INSTALL_MKG.sh
@@ -12,4 +12,5 @@ cd ./src
 echo "##################"
 echo "################## INNSIDE OF $(pwd)"
 echo "##################"
+python embed.py --embed_texts --do_umap
 python embed.py --do_umap --embed_images --images_path ./images
